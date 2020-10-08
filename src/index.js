3f39d4962a8db12c62185b48277d2026811eac0c
Yuriy Khomenko <kaniv10@gmail.com>
2020-10-08 21:31:16 +0300

    added gitignore

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ const qclone = require('./qclone');
 const qcloneStrict = require('./qcloneStrict');
 const qcloneCircular = require('./qcloneCircular');
 const qcloneStrictCircular = require('./qcloneStrictCircular');
-const qcloneJson = require('./qcloneJson');
 
 module.exports = {
   qclone,
   qcloneStrict,
   qcloneCircular,
   qcloneStrictCircular,
-  qcloneJson,
   clone: qclone,
   strict: qcloneStrict,
   circular: qcloneCircular,
-  strictCircular: qcloneStrictCircular,
-  json: qcloneJson
+  strictCircular: qcloneStrictCircular
 };
